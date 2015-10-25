cbb14544a5afc67076766e4ef229fcff428417f1
ir0ny1 <dave@ir0ny.com>
2015-10-25 01:59:47 -0400

    Playing with code climate

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -2,17 +2,20 @@
 
 use ir0ny1\Validator\Validator;
 
+/**
+ * Class ValidatorTest
+ */
 class ValidatorTest extends PHPUnit_Framework_TestCase
 {
 
     public function testRequiredExists()
     {
         $data = [
-            'name'  => 'Dave'
+            'name' => 'Dave'
         ];
 
         $rules = [
-            'name'  => 'required'
+            'name' => 'required'
         ];
 
         $val = new Validator($data, $rules);
@@ -24,15 +27,121 @@ class ValidatorTest extends PHPUnit_Framework_TestCase
 
     public function testRequiredDoesNotExist()
     {
-        $data = [];
+        $data = [
+            'name'  =>  ''
+        ];
         $rules = [
-            'name'  => 'required'
+            'name'  =>  'required'
         ];
 
         $val = new Validator($data, $rules);
 
         $val->validate();
 
-        $this->assertTrue(!$val->valid());
+        $this->assertFalse($val->valid());
+    }
+
+    public function testEqualToPasses()
+    {
+        $data = [
+            'a' => 'a',
+        ];
+
+        $rules = [
+            'a' => 'equalTo:a'
+        ];
+
+        $val = new Validator($data, $rules);
+
+        $val->validate();
+
+        $this->assertTrue($val->valid());
+    }
+
+    public function testEqualToFails()
+    {
+        $data = [
+            'a' => 'b',
+        ];
+
+        $rules = [
+            'a' => 'equalTo:a'
+        ];
+
+        $val = new Validator($data, $rules);
+
+        $val->validate();
+
+        $this->assertFalse($val->valid());
+    }
+
+    public function testSameAsPasses()
+    {
+        $data = [
+            'a' => 'a',
+            'b' => 'a'
+        ];
+
+        $rules = [
+            'b' => 'sameAs:a'
+        ];
+
+        $val = new Validator($data, $rules);
+
+        $val->validate();
+
+        $this->assertTrue($val->valid());
+    }
+
+    public function testSameAsFails()
+    {
+        $data = [
+            'a' => 'a',
+            'b' => 'c'
+        ];
+
+        $rules = [
+            'a' => 'sameAs:b',
+        ];
+
+        $val = new Validator($data, $rules);
+
+        $val->validate();
+
+        $this->assertFalse($val->valid());
+    }
+
+    public function testMaxLengthPasses()
+    {
+        $data = [
+            'name'  =>  'dave'
+        ];
+
+        $rules = [
+            'name'  =>  'maxLength:4'
+        ];
+
+        $val = new Validator($data, $rules);
+
+        $val->validate();
+
+        $this->assertTrue($val->valid());
+    }
+
+    public function testMaxLengthFails()
+    {
+        $data = [
+            'name'  =>  'david'
+        ];
+
+        $rules = [
+            'name'  =>  'maxLength:4'
+        ];
+
+        $val = new Validator($data, $rules);
+
+        $val->validate();
+
+        $this->assertFalse($val->valid());
     }
 }
